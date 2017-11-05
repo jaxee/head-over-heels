6df27683d5269aceb86a951f9f66138f45acc5c0
Jackie Ellis <jackie.ellis@hotmail.com>
2017-11-05 17:04:38 -0500

    Split screen functionality added

---
+++ head_over_heels_beta/Assets/Scripts/FlipScreenMechanic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlipScreenMechanic : MonoBehaviour {
+
+	public Button flipScreen;
+	public Camera lunaCam;
+	public Camera solCam;
+	bool isLunaCam = true;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = flipScreen.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	void TaskOnClick() {
+		Debug.Log("FLIP THE DAMN SCREEN!");
+
+		if (isLunaCam) {
+
+			isLunaCam = false;
+		} else {
+
+			isLunaCam = true;
+		}
+	}
+}
