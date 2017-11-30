d003af09e8ed4714c2c5240e8d3ec0aec09be953
Jackie Ellis <jackie.ellis@hotmail.com>
2017-11-29 23:22:37 -0500

    Added mystery boxes

---
+++ head_over_heels_beta/Assets/Scripts/AnimateBounce.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateBounce : MonoBehaviour {
+
+	public bool Move = true;
+	public Vector3 MoveVector = Vector3.up;
+	public float MoveRange;
+	public float MoveSpeed;
+
+	private AnimateBounce bounceObject;
+	Vector3 startPosition;
+
+	void Start() { 
+
+		bounceObject = this;
+		startPosition = bounceObject.transform.position;
+
+	}
+
+	void Update() {
+
+		if(Move)
+			bounceObject.transform.position = startPosition + MoveVector * (MoveRange * Mathf.Sin(Time.timeSinceLevelLoad * MoveSpeed));
+
+	}
+}
