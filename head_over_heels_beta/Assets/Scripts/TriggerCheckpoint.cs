2d819056c4a417b3da2b673ce60891d53a0136bd
Josh Chartrand <joshuachartrand@hotmail.com>
2017-12-07 13:45:26 -0500

    Added signs and checkpoints

---
+++ head_over_heels_beta/Assets/Scripts/TriggerCheckpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheckpoint : MonoBehaviour {
+
+	private bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag == "Player" && !triggered) {
+			triggered = true;
+			SpriteRenderer[] checkpoints = GetComponentsInChildren<SpriteRenderer> ();
+			foreach (SpriteRenderer renderer in checkpoints) {
+				renderer.enabled = !renderer.enabled;
+			}
+		}
+	}
+}
