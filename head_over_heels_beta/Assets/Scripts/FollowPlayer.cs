14db893406a433ec9db20edb7c5953fa6eab0ef4
Josh Chartrand <joshuachartrand@hotmail.com>
2017-12-06 19:10:08 -0500

    Fixed bugs with moving platform

--- head_over_heels_beta/Assets/Scripts/FollowPlayer.cs
+++ head_over_heels_beta/Assets/Scripts/FollowPlayer.cs
@@ -14,6 +14,8 @@ public class FollowPlayer : MonoBehaviour {
 	private float horizontalPosition;
 	private float verticalPosition;
 	private Vector2 startPosition;
+	private Vector2 lastPlatformPosition;
+	private Vector2 currentPlatformPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -27,43 +29,83 @@ public class FollowPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// Jumping up
-		if (playerController.playerRigidBody.velocity.y > 0) {
-			if (Mathf.Abs (player.transform.position.y - verticalPosition) < minVerticalDistance) {
-				verticalPosition = player.transform.position.y + minVerticalDistance;
-			}
-		}
+		// If the player is moving on a platform, follow the platform
+		if (player.transform.parent) {
 
-		// Falling down
-		if (playerController.playerRigidBody.velocity.y < 0 && transform.position.y > (startPosition.y)) {
-			if (Mathf.Abs (player.transform.position.y - verticalPosition) > maxVerticalDistance) {
-				verticalPosition = player.transform.position.y + maxVerticalDistance;
-			}
-		}
+			currentPlatformPosition = player.transform.parent.position;
 
-		// Moving left
-		if (playerController.playerRigidBody.velocity.x < 0) {
-			if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
-				if (player.transform.position.x + maxHorizontalDistance < transform.position.x) {
-					horizontalPosition = player.transform.position.x + maxHorizontalDistance;
+			// Moving up
+			if (currentPlatformPosition.y > lastPlatformPosition.y) {
+				if (Mathf.Abs (player.transform.position.y - verticalPosition) < minVerticalDistance) {
+					verticalPosition = player.transform.position.y + minVerticalDistance;
 				}
 			}
-			else {
-				horizontalPosition = transform.position.x;
-			}
-		}
-		// Moving right
-		if (playerController.playerRigidBody.velocity.x > 0) {
-			if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
-				if (player.transform.position.x - maxHorizontalDistance > transform.position.x) {
-					horizontalPosition = player.transform.position.x - maxHorizontalDistance;
+
+			// Moving down
+			if (currentPlatformPosition.y < lastPlatformPosition.y && transform.position.y > (startPosition.y)) {
+				if (Mathf.Abs (player.transform.position.y - verticalPosition) > maxVerticalDistance) {
+					verticalPosition = player.transform.position.y + maxVerticalDistance;
 				}
 			}
-			else {
-				horizontalPosition = transform.position.x;
+
+			// Moving left
+			if (currentPlatformPosition.x < lastPlatformPosition.x) {
+				if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
+					if (player.transform.position.x + maxHorizontalDistance < transform.position.x) {
+						horizontalPosition = player.transform.position.x + maxHorizontalDistance;
+					}
+				} else {
+					horizontalPosition = transform.position.x;
+				}
+			}
+			// Moving right
+			if (currentPlatformPosition.x > lastPlatformPosition.x) {
+				if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
+					if (player.transform.position.x - maxHorizontalDistance > transform.position.x) {
+						horizontalPosition = player.transform.position.x - maxHorizontalDistance;
+					}
+				} else {
+					horizontalPosition = transform.position.x;
+				}
+			}
+
+			lastPlatformPosition = currentPlatformPosition;
+		} else {
+			// Jumping up
+			if (playerController.playerRigidBody.velocity.y > 0) {
+				if (Mathf.Abs (player.transform.position.y - verticalPosition) < minVerticalDistance) {
+					verticalPosition = player.transform.position.y + minVerticalDistance;
+				}
+			}
+
+			// Falling down
+			if (playerController.playerRigidBody.velocity.y < 0 && transform.position.y > (startPosition.y)) {
+				if (Mathf.Abs (player.transform.position.y - verticalPosition) > maxVerticalDistance) {
+					verticalPosition = player.transform.position.y + maxVerticalDistance;
+				}
+			}
+
+			// Moving left
+			if (playerController.playerRigidBody.velocity.x < 0) {
+				if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
+					if (player.transform.position.x + maxHorizontalDistance < transform.position.x) {
+						horizontalPosition = player.transform.position.x + maxHorizontalDistance;
+					}
+				} else {
+					horizontalPosition = transform.position.x;
+				}
+			}
+			// Moving right
+			if (playerController.playerRigidBody.velocity.x > 0) {
+				if (Mathf.Abs (player.transform.position.x - transform.position.x) > maxHorizontalDistance) {
+					if (player.transform.position.x - maxHorizontalDistance > transform.position.x) {
+						horizontalPosition = player.transform.position.x - maxHorizontalDistance;
+					}
+				} else {
+					horizontalPosition = transform.position.x;
+				}
 			}
 		}
-
 
 			
 		transform.position = new Vector3 (horizontalPosition, verticalPosition, transform.position.z);
